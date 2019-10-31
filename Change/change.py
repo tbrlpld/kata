dcc784ef3f4050a1682bb98da7bb8e73af17c120
Tibor Leupold <tibor@lpld.io>
2019-10-30 18:26:26 -0700

    Added algorithm

--- Change/change.py
+++ Change/change.py
@@ -4,8 +4,60 @@
 
 
 
+class Coin(object):
+    """
+    Coin class to create coin objects.
+
+    A coin can be created with a given name and value. Once the name and value
+    have been defined, the name and value can not be changed
+    """
+
+    def __init__(self, name: str, cent_value: int):
+        """Initialize a coin."""
+        self._name = name
+        self._cent_value = cent_value
+
+    @property
+    def name(self):
+        """Name of the coin."""
+        return self._name
+
+    @property
+    def value(self):
+        """Value of the coin."""
+        return self._cent_value
+
+    def __repr__(self):
+        return "{0} ({1} cent(s))".format(self.name, self.value)
+
+
+PENNY = Coin(name="penny", cent_value=1)
+NICKLE = Coin(name="nickel", cent_value=5)
+DIME = Coin(name="dime", cent_value=10)
+QUARTER = Coin(name="quarter", cent_value=25)
+
+COINS = sorted(
+    [PENNY, NICKLE, DIME, QUARTER], key=lambda coin: coin.value, reverse=True)
+
+
 def change(cents: int):
     """Return the least number of coins for the given cent value."""
     coins_to_return = {}
-    coins_to_return["penny"] = 1
+
+    target_value_of_coins = cents
+    combined_value_of_coins = 0
+    # Iterate through the coins starting with the largest
+    for coin in COINS:
+        current_coin = coin
+        # Add coin if result still smaller than or equal to target
+        while (
+            combined_value_of_coins + current_coin.value
+            <= target_value_of_coins
+        ):
+            if coins_to_return.get(coin.name) is None:
+                coins_to_return[current_coin.name] = 1
+            else:
+                coins_to_return[current_coin.name] += 1
+            combined_value_of_coins += current_coin.value
+
     return coins_to_return
--- Change/test_change.py
+++ Change/test_change.py
@@ -12,15 +12,16 @@ from change import change
     (
         (1, {"penny": 1}),
         (3, {"penny": 3}),
-        (5, {"nickle": 1}),
-        (6, {"nickle": 1, "penny": 1}),
+        (5, {"nickel": 1}),
+        (6, {"nickel": 1, "penny": 1}),
+        (11, {"dime": 1, "penny": 1}),
+        (15, {"dime": 1, "nickel": 1}),
     ),
 )
 def test_change(cents, expected_change):
     """Test change function."""
-    returned_change = change(cents=1)
-    assert returned_change.get("penny") == expected_change.get("penny")
-    assert returned_change.get("nickle") == expected_change.get("nickle")
-    assert returned_change.get("dime") == expected_change.get("dime")
+    returned_change = change(cents)
     assert returned_change.get("quarter") == expected_change.get("quarter")
-
+    assert returned_change.get("dime") == expected_change.get("dime")
+    assert returned_change.get("nickel") == expected_change.get("nickel")
+    assert returned_change.get("penny") == expected_change.get("penny")
