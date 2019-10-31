7bb1dfdfa2c0375e0a79f66291e10b996bf47354
Tibor Leupold <tibor@lpld.io>
2019-10-30 17:57:13 -0700

    Added tests for more values and multiple coins

--- Change/test_change.py
+++ Change/test_change.py
@@ -2,10 +2,25 @@
 
 """Tests for the change program."""
 
+import pytest
+#
 from change import change
 
 
-def test_change():
+@pytest.mark.parametrize(
+    ("cents", "expected_change"),
+    (
+        (1, {"penny": 1}),
+        (3, {"penny": 3}),
+        (5, {"nickle": 1}),
+        (6, {"nickle": 1, "penny": 1}),
+    ),
+)
+def test_change(cents, expected_change):
     """Test change function."""
     returned_change = change(cents=1)
-    assert returned_change["penny"] == 1
+    assert returned_change.get("penny") == expected_change.get("penny")
+    assert returned_change.get("nickle") == expected_change.get("nickle")
+    assert returned_change.get("dime") == expected_change.get("dime")
+    assert returned_change.get("quarter") == expected_change.get("quarter")
+
