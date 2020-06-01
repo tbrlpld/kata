96bdbc089f41944f1fa292ddfbf3a4517cf77b71
Tibor Leupold <tibor@lpld.io>
2020-05-31 20:28:24 -1000

    Add isDivisible func

--- fizzbuzz-js/fizzbuzz.js
+++ fizzbuzz-js/fizzbuzz.js
@@ -1,7 +1,23 @@
-function main (start, end) {
+
+function isDivisible (dividend, divisior) {
+  const rest = dividend % divisior
+  if (rest === 0) {
+    return true
+  } else {
+    return false
+  }
+}
+
+exports.isDivisible = isDivisible
+
+function main (start = 1, end = 100) {
+  console.log('Main')
+  for (let i = start; i <= end; i++) {
+    console.log(i)
+  }
   return []
 }
 
 exports.main = main
 
-main()
+// main()
--- fizzbuzz-js/fizzbuzz.test.js
+++ fizzbuzz-js/fizzbuzz.test.js
@@ -1,15 +1,33 @@
 const fizzbuzz = require('./fizzbuzz')
 
-describe('Main function', () => {
-  test('1 through 10', () => {
-    expect(fizzbuzz.main(0, 10)).toEqual(
-      ['1', '2', 'fizz', '4', 'buzz', '6', '7', '8', 'fizz', 'buzz']
-    )
+describe('Is divisible by', () => {
+  test('6 is divisible by 3', () => {
+    expect(fizzbuzz.isDivisible(6, 3)).toEqual(true)
   })
 
-  test('90 through 100', () => {
-    expect(fizzbuzz.main(90, 100)).toEqual(
-      ['fizzbuzz', '91', '92', 'fizz', '94', 'buzz', 'fizz', '97', '98', 'fizz', 'buzz']
-    )
+  test('7 is not divisible by 3', () => {
+    expect(fizzbuzz.isDivisible(7, 3)).toEqual(false)
+  })
+
+  test('10 is not divisible by 3', () => {
+    expect(fizzbuzz.isDivisible(10, 3)).toEqual(false)
+  })
+
+  test('4 is divisible by 2', () => {
+    expect(fizzbuzz.isDivisible(4, 2)).toEqual(true)
   })
 })
+
+// describe('Main function', () => {
+//   test('1 through 10', () => {
+//     expect(fizzbuzz.main(0, 10)).toEqual(
+//       ['1', '2', 'fizz', '4', 'buzz', '6', '7', '8', 'fizz', 'buzz']
+//     )
+//   })
+
+//   test('90 through 100', () => {
+//     expect(fizzbuzz.main(90, 100)).toEqual(
+//       ['fizzbuzz', '91', '92', 'fizz', '94', 'buzz', 'fizz', '97', '98', 'fizz', 'buzz']
+//     )
+//   })
+// })
