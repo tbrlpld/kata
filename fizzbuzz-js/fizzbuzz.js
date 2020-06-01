a0db839aaab018ee08f981d3fb99117c3b401426
Tibor Leupold <tibor@lpld.io>
2020-05-31 18:47:29 -1000

    Add fizzbuzz-js kata

---
+++ fizzbuzz-js/fizzbuzz.js
@@ -0,0 +1,7 @@
+function main () {
+  return []
+}
+
+module.exports = main
+
+main()
---
+++ fizzbuzz-js/fizzbuzz.test.js
@@ -0,0 +1,5 @@
+const fizzbuzz = require('./fizzbuzz')
+
+test('Dummy test', () => {
+  expect(fizzbuzz()).toEqual([])
+})
